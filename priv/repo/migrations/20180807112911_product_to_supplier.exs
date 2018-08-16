9ce2d95b08903a0952e0a31d47d14751349886df
bjornum <37101863+bjornum@users.noreply.github.com>
2018-08-16 10:33:22 +0200

    removed old mix files

--- priv/repo/migrations/20180807112911_product_to_supplier.exs
+++
@@ -1,9 +0,0 @@
-defmodule Prios.Repo.Migrations.ProductToSupplier do
-  use Ecto.Migration
-
-  def change do
-    alter table(:products) do
-      add :supplier_id, references (:suppliers)
-    end
-  end
-end
