c9e4e257fc9bb8c03584577b0a5ad8cdf0711150
bjornum <37101863+bjornum@users.noreply.github.com>
2018-08-17 13:26:12 +0200

    Updated the seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,14 +11,14 @@
 # and so on) as they will fail if something goes wrong.
 
 # Users and login
-user = %{name: "Jane Doe", fname: "Jane", lname: "Doe", phone: "123456789", postal_code: "7715", address: "Kongensgate 35", country: "Norway", city: "Steinkjer"}
+user = %{fullname: "Jane Doe", fname: "Jane", lname: "Doe", phone: "123456789", postal_code: "7715", address: "Kongensgate 35", country: "Norway", city: "Steinkjer"}
 login = %{email: "test1@mail.com", password_hash: "notimplemented", isadmin: true}
 Prios.Identity.create_user(user, login)
 
-user = %{fname: "Test2", lname: "Testmann"}
+user = %{fullname: "Petter Testmann", fname: "Petter", lname: "Testmann", phone: "111222333", postal_code: "7799", address: "Oslo Veg 32", country: "Norway", city: "Oslo"}
 login = %{email: "test2@mail.com", password_hash: "notimplemented", isadmin: true}
 Prios.Identity.create_user(user, login)
-user = %{fname: "Test3", lname: "Testmann"}
+user = %{fullname: "Bob Smith", fname: "Bob", lname: "Smith", phone: "999999999", postal_code: "55692", address: "Mississippi Street 2", country: "America", city: "Mississippi"}
 login = %{email: "test3@mail.com", password_hash: "notimplemented", isadmin: true}
 Prios.Identity.create_user(user, login)
 
