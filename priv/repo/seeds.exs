db5d3d9cacb02b60e252999efd5b61863dbe75e7
bjornum <37101863+bjornum@users.noreply.github.com>
2018-08-17 13:15:48 +0200

    Edited some dummy data into user 1

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,7 +11,7 @@
 # and so on) as they will fail if something goes wrong.
 
 # Users and login
-user = %{fname: "Test1", lname: "Testmann", phone: "123456789", postal_code: "7715"}
+user = %{fname: "Jane", lname: "Doe", phone: "123456789", postal_code: "7715", address: "Kongensgate 35", country: "Norway", city: "Steinkjer"}
 login = %{email: "test1@mail.com", password_hash: "notimplemented", isadmin: true}
 Prios.Identity.create_user(user, login)
 
